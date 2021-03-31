42e16c1c6018f34338711c974196ee1cb26028e2
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-03-31 10:08:01 +0530

    Refactor: Accept input from user

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pip._vendor.distlib.compat import raw_input
 
 
-def swaps(array):
+def swaps(inputArray):
     noOfSwaps = 0
-    for i in range(len(array)):
-        if array[i] != i+1:
+    for i in range(len(inputArray)):
+        if inputArray[i] != i+1:
             noOfSwaps += 1
-            indexOfElement = array.index(i+1)
-            array[i], array[indexOfElement] = i+1, array[i]
+            indexOfElement = inputArray.index(i + 1)
+            inputArray[i], inputArray[indexOfElement] = i + 1, inputArray[i]
     return noOfSwaps
 
 
 if __name__ == '__main__':
-    noOfSwaps = swaps([7, 1, 3, 2, 4, 5, 6])
+    noOfElements = input()
+    inputArray = raw_input()
+    array = [int(x) for x in inputArray.split(",")]
+    noOfSwaps = swaps(array)
     print(noOfSwaps)
